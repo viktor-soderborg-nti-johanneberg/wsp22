53565c96aaca3118288552ce2be2676d006402f7
viktor-soderborg-nti-johanneberg <74763822+viktor-soderborg-nti-johanneberg@users.noreply.github.com>
2022-03-16 16:43:23 +0100

    v.11
    Changes made during v.11

--- app.rb
+++ app.rb
@@ -1,44 +1,42 @@
 require 'sinatra'
 require 'slim'
-require 'sqlite3'
-require 'bcrypt'
 
-# Todo: milestones
+require_relative 'model.rb'
 
 # https://github.com/pure-css/pure/blob/master/site/static/layouts/marketing/index.html
 # https://purecss.io/layouts/marketing/
+
+#Test account: {Username: Bob, Password: 7}
 
 enable :sessions
 logged_in = false
 db = ""
 admin = false
 
-before do
-  db = SQLite3::Database.new('db/slutprojekt.db')
-  db.results_as_hash = true
-end
-
 get('/') do
   slim(:index, locals:{logged_in:logged_in, admin:admin})
 end
 
 get('/register') do
-  slim(:register, locals:{logged_in:logged_in})
+  if logged_in
+    slim(:logout_error, locals:{logged_in:logged_in, admin:admin})
+  else
+    slim(:register, locals:{logged_in:logged_in, admin:admin})
+  end
 end
 
 get('/showlogin') do
-  slim(:login, locals:{logged_in:logged_in})
+  if logged_in
+    slim(:logout_error, locals:{logged_in:logged_in, admin:admin})
+  else
+    slim(:login, locals:{logged_in:logged_in, admin:admin})
+  end
 end
 
 post('/login') do
   username = params[:username]
   password = params[:password]
-  result = db.execute("SELECT * FROM user WHERE username = ?",username).first
-  pwdigest = result["pwdigest"]
-  id = result["id"]
-  role = result["role"]
-
-  if BCrypt::Password.new(pwdigest) == password
+  if checkLogin(username)
     session[:id] = id
     session[:username] = username
     logged_in = true
@@ -48,33 +46,32 @@ post('/login') do
     redirect('/activities')
   else
     logged_in = false
-    "FEL LÖSENORD!"
+    redirect('/passerror')
   end
 end
 
 get('/activities') do
   id = session[:id].to_i
-  activities = db.execute('SELECT * FROM activities WHERE user_id = ?',id)
+  activities = getActivities(id)
   if logged_in
     slim(:"activities/index", locals:{activities:activities, logged_in:logged_in, admin:admin})
   else
-    slim(:error, locals:{activities:activities, logged_in:logged_in})
+    slim(:login_error, locals:{activities:activities, logged_in:logged_in})
   end
 end
 
 post('/activities/:id/delete') do
   id = params[:id].to_i
-  db.execute('DELETE FROM activities WHERE id = ?',id)
+  deleteActivity(id)
   redirect('/activities')
 end
 
 post('/activities/new') do
   name = params[:name]
-  hrs = params[:hrs]
-  if db.execute('SELECT * FROM activities WHERE user_id = ?', session[:id]).length == 0
-    db.execute('INSERT INTO usermilerel (user_id, milestone_id) VALUES (?,?)', session[:id], 1)
-  end
-  db.execute("INSERT INTO activities (name, user_id, time) VALUES (?,?,?)",name, session[:id], hrs)
+  time = params[:time]
+  milestones = getMilestones(session[:id])
+  updateMilestones(session[:id], time, milestones)
+  createActivity(name, session[:id], time)
   redirect('/activities')
 end
 
@@ -82,14 +79,19 @@ post('/activities/:id/update') do
   id = params[:id].to_i
   name = params[:name]
   time = params[:time]
-  db.execute("UPDATE activities SET name=?,time=? WHERE id =?",name,time,id)
+  milestones = getMilestones(session[:id])
+  updateMilestones(session[:id], time, milestones)
   redirect('/activities')
 end
 
 get('/activities/:id/edit') do
   id = params[:id].to_i
-  result = db.execute("SELECT * FROM activities WHERE id = ?",id).first 
-  slim(:"/activities/edit", locals:{result:result, logged_in:logged_in, admin:admin})
+  result = editActivity(id)
+  if logged_in
+    slim(:"/activities/edit", locals:{result:result, logged_in:logged_in, admin:admin})
+  else
+    slim(:login_error, locals:{result:result, logged_in:logged_in, admin:admin, username:session["username"]})
+  end
 end
 
 post('/users/new') do
@@ -98,9 +100,7 @@ post('/users/new') do
   password_confirm = params[:password_confirm]
 
   if password == password_confirm
-    password_digest = BCrypt::Password.create(password)
-    db.execute('INSERT INTO user (username,pwdigest,role) VALUES (?,?,?)',username,password_digest,"member")
-    session[:id] = db.execute('SELECT id FROM user WHERE username = ?', username).first["id"]
+    session[:id] = registerUser(username, password)
     logged_in = true
     session[:username] = username
     redirect('/')
@@ -111,13 +111,13 @@ end
 
 get('/user/:id') do
   id = params[:id].to_i
-  user = db.execute('SELECT * FROM user WHERE id = ?', id).first
-  milestones = db.execute('SELECT * FROM usermilerel INNER JOIN milestones ON usermilerel.milestone_id = milestones.id WHERE user_id = ?',id)
-  allmile = db.execute('SELECT * FROM milestones')
+  user = getUser(id)
+  milestones = showMilestones(id)
+  allmile = allMilestones()
   if logged_in
     slim(:"users/user", locals:{logged_in:logged_in, username:user["username"], milestones:milestones, allmile:allmile, admin:admin, user:user})
   else
-    slim(:error, locals:{logged_in:logged_in, username:user["username"], milestones:milestones, allmile:allmile})
+    slim(:login_error, locals:{logged_in:logged_in, username:user["username"], milestones:milestones, allmile:allmile})
   end
 end
 
@@ -129,36 +129,40 @@ end
 
 get('/admin') do
   if logged_in && admin
-    users = db.execute('SELECT * FROM user')
+    users = getUsers()
     slim(:admin, locals:{logged_in:logged_in, admin:admin, users:users})
   else
-    slim(:error, locals:{logged_in:logged_in})
+    slim(:login_error, locals:{logged_in:logged_in})
   end
 end
 
 get('/members') do
   if logged_in
-    users = db.execute('SELECT * FROM user')
+    users = getUsers()
     slim(:"users/members", locals:{logged_in:logged_in, admin:admin, users:users})
   else
-    slim(:error, locals:{logged_in:logged_in})
+    slim(:login_error, locals:{logged_in:logged_in})
   end
 end
 
 post('/users/:id/delete') do
+  deleteUser(id)
   id = params[:id].to_i
-  db.execute('DELETE FROM user WHERE id = ?', id)
   redirect('/admin')
 end
 
 post('/users/:id/edit') do
   id = params[:id].to_i
   role = params[:role]
-  db.execute('UPDATE user SET role = ? WHERE id = ?', role, id)
+  editUser(id, role)
   if role == "admin"
     admin = true
   else
     admin = false
   end
   redirect('/admin')
+end
+
+get('/passerror') do
+  slim(:wrong_password, locals:{logged_in:logged_in})
 end
---
+++ model.rb
@@ -0,0 +1,123 @@
+require 'sqlite3'
+require 'bcrypt'
+
+def checkLogin(username)
+    db = SQLite3::Database.new('db/slutprojekt.db')
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM user WHERE username = ?", username).first
+    pwdigest = result["pwdigest"]
+    id = result["id"]
+    role = result["role"]
+  
+    if BCrypt::Password.new(pwdigest) == password
+        return true
+    else
+        return false
+    end
+end
+
+def getActivities(id)
+    db = SQLite3::Database.new('db/slutprojekt.db')
+    db.results_as_hash = true
+    return db.execute('SELECT * FROM activities WHERE user_id = ?', id)
+end
+
+def deleteActivity(id)
+    db = SQLite3::Database.new('db/slutprojekt.db')
+    db.results_as_hash = true
+    db.execute('DELETE FROM activities WHERE id = ?', id)
+    return nil
+end
+
+def getMilestones(id)
+    db = SQLite3::Database.new('db/slutprojekt.db')
+    db.results_as_hash = true
+    return db.execute('SELECT * FROM usermilerel WHERE user_id = ?', id)
+end
+
+def updateMilestones(id, time, milestones)
+    db = SQLite3::Database.new('db/slutprojekt.db')
+    db.results_as_hash = true
+    if db.execute('SELECT * FROM activities WHERE user_id = ?', id).length == 0
+        db.execute('INSERT INTO usermilerel (user_id, milestone_id) VALUES (?,?)', id, 1)
+    end
+    if milestones.length < 2 && time.to_i >= 100
+        db.execute('INSERT INTO usermilerel (user_id, milestone_id) VALUES (?,?)', id, 2)
+    end
+    if milestones.length < 3 && time.to_i >= 200
+        db.execute('INSERT INTO usermilerel (user_id, milestone_id) VALUES (?,?)', id, 3)
+    end
+    if milestones.length < 4 && time.to_i >= 300
+        db.execute('INSERT INTO usermilerel (user_id, milestone_id) VALUES (?,?)', id, 4)
+    end
+    if milestones.length < 5 && time.to_i >= 400
+        db.execute('INSERT INTO usermilerel (user_id, milestone_id) VALUES (?,?)', id, 5)
+    end
+    if milestones.length < 6 && time.to_i >= 500
+        db.execute('INSERT INTO usermilerel (user_id, milestone_id) VALUES (?,?)', id, 6)
+    end
+end
+
+def createActivity(name, id, time)
+    db = SQLite3::Database.new('db/slutprojekt.db')
+    db.results_as_hash = true
+    db.execute("INSERT INTO activities (name, user_id, time) VALUES (?,?,?)", name, id, time)
+end
+
+def updateActivity(name, time, id)
+    db = SQLite3::Database.new('db/slutprojekt.db')
+    db.results_as_hash = true
+    db.execute("UPDATE activities SET name=?,time=? WHERE id =?", name, time, id)
+end
+
+def editActivity(id)
+    db = SQLite3::Database.new('db/slutprojekt.db')
+    db.results_as_hash = true
+    return db.execute("SELECT * FROM activities WHERE id = ?",id).first 
+end
+
+def registerUser(username, password)
+    db = SQLite3::Database.new('db/slutprojekt.db')
+    db.results_as_hash = true
+    password_digest = BCrypt::Password.create(password)
+    db.execute('INSERT INTO user (username,pwdigest,role) VALUES (?,?,?)',username,password_digest,"member")
+    return db.execute('SELECT id FROM user WHERE username = ?', username).first["id"]
+end
+
+def getUser(id)
+    db = SQLite3::Database.new('db/slutprojekt.db')
+    db.results_as_hash = true
+    return db.execute('SELECT * FROM user WHERE id = ?', id).first
+end
+
+def showMilestones(id)
+    db = SQLite3::Database.new('db/slutprojekt.db')
+    db.results_as_hash = true
+    return db.execute('SELECT * FROM usermilerel INNER JOIN milestones ON usermilerel.milestone_id = milestones.id WHERE user_id = ?',id)
+end
+
+def allMilestones()
+    db = SQLite3::Database.new('db/slutprojekt.db')
+    db.results_as_hash = true
+    return db.execute('SELECT * FROM milestones')
+end
+
+def getUsers()
+    db = SQLite3::Database.new('db/slutprojekt.db')
+    db.results_as_hash = true
+    return db.execute('SELECT * FROM user')
+end
+
+def deleteUser(id)
+    db = SQLite3::Database.new('db/slutprojekt.db')
+    db.results_as_hash = true
+    db.execute('DELETE FROM user WHERE id = ?', id)
+    return nil
+end
+
+def editUser(id)
+    db = SQLite3::Database.new('db/slutprojekt.db')
+    db.results_as_hash = true
+    db.execute('UPDATE user SET role = ? WHERE id = ?', role, id)
+    return nil
+end
